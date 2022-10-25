9262f12d7a5a0a20e692509addf526bcca4a0029
minhminh191000 <minh2k3k4k@gmail.com>
2022-10-25 13:33:08 +0700

    update

---
+++ controller/job_registration.py
@@ -0,0 +1,38 @@
+from app import app ,db,get_current_user
+from flask import jsonify,request
+from flask_jwt_extended import jwt_required,get_jwt_identity
+
+import json
+# from app import db,app
+# from model.personal_information import *
+
+class JobRegistration:
+
+    def get(self):
+        pass
+    def create(self):
+        pass
+    def delete(self):
+        pass
+
+
+            
+
+
+
+
+
+jobregistration = JobRegistration()
+# # app.ad
+# app.add_enpoint("/infomation/create","create_infomation",infomation.create,methods=["POST"])
+# app.add_enpoint("/infomation/update","update_infomation",infomation.update,methods=["PUT"])
+# app.add_enpoint("/jobinformation/get_all","get_all",jobinformation.get_all_paging,methods=["GET"])
+# app.add_enpoint("/jobinformation/detail_job/<int:id>","detail_job",jobinformation.detail_job,methods=["GET"])
+# app.add_enpoint("/jobinformation/search_job","search_job",jobinformation.search_job,methods=["GET"])
+# app.add_enpoint("/jobinformation/get_all","get_all",jobinformation.get_all_paging,methods=["GET"])
+# app.add_enpoint("/infomation/delete","delete_infomation",infomation.get,methods=["POST"])
+# get_all_company
+
+            
+
+
