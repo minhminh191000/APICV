33f3e682b465ff8e2703dd7b641918a0d6ae5ecf
minhminh191000 <minh2k3k4k@gmail.com>
2022-11-05 01:15:37 +0700

    update expiring

--- app.py
+++ app.py
@@ -34,20 +34,20 @@ flask_app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
 jwt = JWTManager(flask_app)
 
 
-@flask_app.after_request
-def refresh_expiring_jwts(response):
-    try:
-        exp_timestamp = get_jwt()["exp"]
-        now = datetime.datetime.now(timezone.utc)
-        # now = datetime.now
-        target_timestamp = datetime.timestamp(now + timedelta(minutes=120))
-        if target_timestamp > exp_timestamp:
-            access_token = create_access_token(identity=get_jwt_identity())
-            set_access_cookies(response, access_token)
-        return response
-    except (RuntimeError, KeyError):
-        # Case where there is not a valid JWT. Just return the original response
-        return response
+# @flask_app.after_request
+# def refresh_expiring_jwts(response):
+#     try:
+#         exp_timestamp = get_jwt()["exp"]
+#         now = datetime.datetime.now(timezone.utc)
+#         # now = datetime.now
+#         target_timestamp = datetime.timestamp(now + timedelta(minutes=120))
+#         if target_timestamp > exp_timestamp:
+#             access_token = create_access_token(identity=get_jwt_identity())
+#             set_access_cookies(response, access_token)
+#         return response
+#     except (RuntimeError, KeyError):
+#         # Case where there is not a valid JWT. Just return the original response
+#         return response
 
 
 @flask_app.route("/login", methods=["POST"])
